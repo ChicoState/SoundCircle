b8b031ad6033250405944a0a2664bf31a596a411
Mustafa <cs.mustafaali@gmail.com>
2024-09-24 14:52:59 -0700

    made migrations for posts and comments tables

--- backend/db/migrations/20240923005829_create_user.ts
+++ backend/db/migrations/20240923005829_create_user.ts
@@ -6,10 +6,19 @@ export async function up(knex: Knex): Promise<void> {
     table.string("username").notNullable();
     table.string("email").unique().notNullable();
     table.string("password").notNullable();
+    table.specificType('posts', 'integer[]');
+    table.specificType('artists', 'text[]');
+    table.specificType('genre', 'text[]');
+    table.specificType('friends', 'integer[]');
+    table.string("image").notNullable();
+    table.string("description");
+    table.integer('location_long').notNullable();  // Added parentheses
+    table.integer('location_lat').notNullable();   // Added parentheses
+    table.integer('radius');
     table.timestamps(true, true);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists("users");  // Drop users table if it exists (rollback)
+  await knex.schema.dropTableIfExists("users");
 }
