6fd272a06c0c7407f482302d8277f1a27ef35507
tooth-juice <kleineckeaustin@gmail.com>
2024-09-24 17:21:25 -0700

    Began work on feed & post containers. Waiting for input on API backend.

--- frontend/src/PageElements/Home/Feed/Posts/post-container.tsx
+++ frontend/src/PageElements/Home/Feed/Posts/post-container.tsx
@@ -1,3 +1,17 @@
 
 
-export {};
+interface PostContainerProperties {
+    userName: string;
+    postContents: string;
+}
+
+function PostContainer({ userName, postContents }: PostContainerProperties) {
+    return (
+        <div className="post-container">
+            <h1>{userName}</h1>
+            <p>{postContents}</p>
+        </div>
+    );
+}
+
+export default PostContainer;
--- frontend/src/PageElements/Home/Feed/feed-container.tsx
+++ frontend/src/PageElements/Home/Feed/feed-container.tsx
@@ -1,14 +1,40 @@
-import { ReactNode } from "react";
-import FeedInputBox from "./input-box";
+import { useEffect, useState } from "react";
+import PostContainer from "./Posts/post-container";
 
-export default function FeedContainer () {
-    return <div></div>;
+const FeedContainer = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    // Attempt to fetch data from the backend
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true); // Start loading, could also link this to an indicator
+                const response = await axios.get('/api/backend-endpoint'); // Get the data from the backend
+                setPosts(response.data);
+            } catch (err) {
+                setError("Failed to fetch post data");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {} // Do loading stuff
+    if (error) {} // Show error stuff
+
+    return (
+        <div className="feed-container">
+            {posts.map(post => (
+                <PostContainer
+                    userName={post.user}
+                    postContents={post.contents}
+                />
+            ))}
+        </div>
+    );
 }
 
-// const FeedContainer = (post) => {
-//     return (
-//         <div>
-//             {post.children}
-//         </div>
-//     );
-// }
+export default FeedContainer;
