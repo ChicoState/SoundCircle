f1dcd37c87c1c3eac5901d3abbbf58de2afe81cd
Mustafa <cs.mustafaali@gmail.com>
2024-09-30 11:31:30 -0700

    modified users tables

--- backend/src/db/migrations/20240923005829_create_user.ts
+++ backend/src/db/migrations/20240923005829_create_user.ts
@@ -3,6 +3,14 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("users", (table) => {
     table.increments("id").primary();
+    table.specificType('artists', 'text[]');       // Array of text
+    table.specificType('genres', 'text[]');        // Array of text
+    table.specificType('friends', 'integer[]');    // Array of integers
+    table.string("image");                         // Image URL or path
+    table.string("description");                   // Description field
+    table.float('longitude');                      // More precision for coordinates
+    table.float('latitude');                       // More precision for coordinates
+    table.float('radius');                         // Float for precision, depends on use case
   });
 }
 
