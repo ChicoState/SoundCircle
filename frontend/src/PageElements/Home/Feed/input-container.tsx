2ab846704c4e4d2dccf80fca99d951232a5cb628
tooth-juice <kleineckeaustin@gmail.com>
2024-09-30 20:24:41 -0700

    Changed organization of Feed Input Box. Began creation of Feed Input Box.

---
+++ frontend/src/PageElements/Home/Feed/input-container.tsx
@@ -0,0 +1,10 @@
+// User facing input for creating a post
+import FeedInputBox from './InputBox/feed-input-box';
+
+const InputContainer = () => {
+    return (
+        <FeedInputBox/>
+    );
+}
+
+export default InputContainer;
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,12 +1,16 @@
 import "./Home.css";
 // import { useNavigate } from "react-router-dom";
 import FeedContainer from "../PageElements/Home/Feed/feed-container";
+import InputContainer from "../PageElements/Home/Feed/input-container";
 
 const Home = () => {
     return (
         <div className="HomePage">
             <div className="HomePage-header"></div>
             <div className="HomePage-columnLeft">
+                <li/>
+                <InputContainer/>
+                <li/>
                 <FeedContainer/>
             </div>
             <div className="HomePage-columnRight">
