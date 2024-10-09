8b8a8f29b8655b3cde3075064a74605a3880a718
tooth-juice <kleineckeaustin@gmail.com>
2024-10-09 15:34:13 -0700

    Added HTTP requests and Database search for new users table.

--- backend/src/Controllers/Users/UserController.ts
+++ backend/src/Controllers/Users/UserController.ts
@@ -1,14 +1,44 @@
 import { Controller, Get, Post, Query, Body, Route, SuccessResponse, Tags } from 'tsoa';
-import { createNewUserProfile } from 'src/Models/Users/user.model';
+import { createNewUserProfile, findUserByName } from 'src/Models/Users/user.model';
 import { User } from '../../../Types/users';
-
 
 @SuccessResponse('200', 'Ok')
 @Route('users')
 @Tags('Users')
 export class UserController extends Controller {
 
-    /*
+    /**
+     * Fetch information about a given user
+     * @returns User type
+     */
+    @Get('/')
+    public async getUserByName(
+        @Query('username') usernameStr: string
+    ): Promise<User> {
+        try {
+            // Attempt to get the user data from db
+            const result = await findUserByName(usernameStr);
+
+            if (result)
+            {
+                // OK
+                this.setStatus(200);
+            } else {
+                // No content
+                this.setStatus(204);
+            }
+
+            return result;
+
+        } catch (error) {
+            console.error('Error in getUserByName: ', error);
+            this.setStatus(500);
+            throw new Error('Failed to get user by name');
+        }
+    }
+
+
+    /**
      * Create a new user and store them into the DB
      */
     @Post('/')
@@ -27,6 +57,7 @@ export class UserController extends Controller {
 
             // Return the user
             return newUser;
+
         } catch (error) {
             // Throw an error to both the backend, HTTP, and frontend
             console.error('Error in postNewUserProfile: ', error);
--- backend/src/Models/Users/user.model.ts
+++ backend/src/Models/Users/user.model.ts
@@ -1,14 +1,33 @@
 import db from '../../db/db';
 import { User } from 'Types/users';
 
+export const findUserByName = async (username: string) => {
+    try {
+        // Attempt to find the username in the database and return the information
+        const foundUser = await db<User>('users')
+            .select('users.username', 'users.userPostIds', 'users.currentLocation')
+            .where('username', username)
+            .first(); // Get the first match
+
+        return foundUser;
+
+    } catch (error) {
+        console.error('Error fetching user by name: ', error);
+        throw new Error('Failed to fetch user by username');
+    }
+}
+
+
 export const createNewUserProfile = async (username: string) => {
     try {
         // Attempt to create a new user and insert them into the DB
-        const newUser = await db<User>('users')
+        const [newUser] = await db<User>('users')
         .insert({
             username: username,
+            userPostIds: [],
+            currentLocation: []
         })
-        .returning('username');
+        .returning(['username', 'userPostIds', 'currentLocation']);
 
         // If there was an error returning a newly made user throw an error
         if (!newUser)
