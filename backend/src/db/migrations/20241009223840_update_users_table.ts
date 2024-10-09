ac83bda10861cf96252ab32fe65586c5bf6eda95
tooth-juice <kleineckeaustin@gmail.com>
2024-10-09 16:00:25 -0700

    Modified knex migration file to alter existing users table.

--- backend/src/db/migrations/20241009223840_update_users_table.ts
+++ backend/src/db/migrations/20241009223840_update_users_table.ts
@@ -1,13 +1,21 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.createTable("users", (table) => {
+    await knex.schema.alterTable("users", (table) => {
         table.increments("id").primary();
         table.string('username').notNullable().unique();
         table.json('userPostIds').defaultTo((JSON.stringify([]))); // Default to empty array
         table.json('currentLocation').defaultTo((JSON.stringify([]))); // Default to empty array
         table.timestamp('created_at').defaultTo(knex.fn.now());
+
+        // We're dropping all of these FOR NOW so we can begin testing User information
+        table.dropColumn('artists');
+        table.dropColumn('genres');
+        table.dropColumn('longitude');
+        table.dropColumn('latitude');
+        table.dropColumn('radius');
+        table.dropColumn('description');
+        table.dropColumn('image');
     });
 }
 
