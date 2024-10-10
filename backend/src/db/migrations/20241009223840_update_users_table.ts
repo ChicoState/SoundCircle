f54391a33293271a3866fd14031a95d763f8d50f
tooth-juice <kleineckeaustin@gmail.com>
2024-10-10 12:02:40 -0700

    Reverted Knex migration. You will need to rollback and then forward to use this!

--- backend/src/db/migrations/20241009223840_update_users_table.ts
+++ backend/src/db/migrations/20241009223840_update_users_table.ts
@@ -1,20 +1,41 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-    // Drop any existing users table
-    await knex.schema.dropTableIfExists("users");
-    // Create a new users table
-    await knex.schema.createTable("users", (table) => {
-        table.increments("id").primary().notNullable();
+    await knex.schema.alterTable("users", (table) => {
         table.string('username').notNullable().unique();
         table.json('userPostIds').defaultTo((JSON.stringify([]))); // Default to empty array
         table.json('currentLocation').defaultTo((JSON.stringify([]))); // Default to empty array
         table.timestamp('created_at').defaultTo(knex.fn.now());
+
+        // We're dropping all of these FOR NOW so we can begin testing User information
+        table.dropColumn('artists');
+        table.dropColumn('genres');
+        table.dropColumn('longitude');
+        table.dropColumn('latitude');
+        table.dropColumn('radius');
+        table.dropColumn('description');
+        table.dropColumn('image');
     });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTableIfExists("users");
+    return knex.schema.alterTable("users", (table) => {
+        // Re-add dropped columns
+        table.specificType('artists', 'text[]');       // Array of text
+        table.specificType('genres', 'text[]');        // Array of text
+        //table.specificType('friends', 'integer[]');    // Array of integers
+        table.string("image");                         // Image URL or path
+        table.string("description");                   // Description field
+        table.float('longitude');                      // More precision for coordinates
+        table.float('latitude');                       // More precision for coordinates
+        table.float('radius');                         // Float for precision, depends on use case
+   
+        // Drop any added columns
+        table.dropColumn('username');
+        table.dropColumn('userPostIds'); // Default to empty array
+        table.dropColumn('currentLocation'); // Default to empty array
+        table.dropColumn('created_at');
+    });
 }
 
