af89a53604f1bcc567df4473ff7ff80a2a86bacf
alexb02h <alexbaroun028@gmail.com>
2024-10-22 21:00:56 -0700

    added user page

--- frontend/src/PageElements/Home/Universal/header.tsx
+++ frontend/src/PageElements/Home/Universal/header.tsx
@@ -1,18 +1,19 @@
 import SearchBar from "../../../Components/Searchbar";
 import SCLogo from "../../../Components/SoundCircle.gif";
 import UserIcon from "../../../Components/UserIconTemp.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header className="bg-gradient-to-r from-periwinkle to-RoyalBlue flex flex-col items-center py-5">
             {/* Left Logo */}
-            <img src={SCLogo} alt="SC Logo" style={{ width: '50px', height: '50px' }} className="absolute top-0 left-0 m-2" />
-            
+            <Link to="/"><img src={SCLogo} alt="SC Logo" style={{ width: '50px', height: '50px' }} className="absolute top-0 left-0 m-2" /></Link>
+
             {/* Center Searchbar */}
             <SearchBar className="w-full max-w-3xl"/>
 
-            {/* Rigth User */}
-            <img src={UserIcon} alt="SC Logo" style={{ width: '50px', height: '50px' }} className="absolute top-0 right-0 m-2" />
+            {/* Right User */}
+            <Link to="/user"><img src={UserIcon} alt="SC Logo" style={{ width: '50px', height: '50px' }} className="absolute top-0 right-0 m-2" /></Link>
         </header>
     );
 }
--- frontend/src/SoundCircle.tsx
+++ frontend/src/SoundCircle.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import SearchResultsPage from './Pages/SearchPage';
+import UserPage from './Pages/User';
 
 const SoundCircle = () => {
   return (
@@ -8,6 +9,7 @@ const SoundCircle = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/Search" element={<SearchResultsPage/>}/>
+        <Route path="/User" element={<UserPage/>}/>
       </Routes>
     </>
   );
