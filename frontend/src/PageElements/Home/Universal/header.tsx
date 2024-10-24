6faaaf0f2644c617f35cae8c05cac7ce78822c80
tooth-juice <kleineckeaustin@gmail.com>
2024-10-23 23:17:24 -0700

    Created universal navigation button. Modified header with grid to properly center item groups. Modified searchbar to allow custom placeholder text.

--- frontend/src/PageElements/Home/Universal/header.tsx
+++ frontend/src/PageElements/Home/Universal/header.tsx
@@ -3,10 +3,13 @@ import SearchBar from "../../../Components/Searchbar";
 import SCLogo from "../../../Components/SoundCircle.gif";
 import UserIcon from "../../../Components/UserIconTemp.png";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import NavigationButton from "../../../Components/Universal/NavigationButton";
 
 const Header = () => {
     const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+    const location = useLocation(); // Get current location (page)
+    const isButtonActive = (path:string) => (location.pathname === path ? "bg-black bg-opacity-50 text-white font-normal" : "");  // Adjust button visuals depending on our current path
 
     useEffect(() => {
         const accessToken = Cookies.get('access_token');
@@ -19,19 +22,21 @@ const Header = () => {
     }, [isUserLoggedIn]);
 
     return (
-        <header className="bg-gradient-to-r from-periwinkle to-RoyalBlue flex items-center h-[60px] px-2">
-            {/* Left Logo */}
-            <div>
-                <Link to="/"><img src={SCLogo} alt="SC Logo" style={{ width: '50px', height: '50px' }}/></Link>
+        <header className="bg-gradient-to-r from-periwinkle to-RoyalBlue grid grid-cols-3 items-center h-[60px]">
+            {/* Left Logo and Buttons */}
+            <div className="flex space-x-0 ml-4">
+                <Link to="/"><img src={SCLogo} alt="SC Logo" style={{ width: '50px', height: '50px' }} className="mt-1 mr-4"/></Link>
+                <NavigationButton buttonText="Home" navigationPath="/" className={`w-[100px] h-[60px] border-r border-l border-black border-opacity-40 ${isButtonActive("/")}`}/>
+                <NavigationButton buttonText="Explore" navigationPath="" className={`w-[100px] h-[60px] border-r border-black border-opacity-40 ${isButtonActive("")}`}/>
             </div>
 
             {/* Center Searchbar */}
-            <div className="flex-grow flex justify-center">
-                <SearchBar className="w-full max-w-2xl" />
+            <div className="flex justify-center">
+                <SearchBar placeHolderText="Search by Artist, Album, Song, Genre, Event, Location" className="w-full"/>
             </div>
 
             {/* Right User/Profile */}
-            <div>
+            <div className="flex justify-end items-center mr-4">
                 {isUserLoggedIn ? (
                     <Link to="/user"><img src={UserIcon} alt="SC Logo" style={{ width: '50px', height: '50px' }} /></Link>
                 ) : (
