b0f9731e1a372aa741adb4d8bfcd9ba03104beb8
Ian Stewart <scrael@protonmail.ch>
2024-10-23 21:15:10 -0700

    Added endpoint to update user location

--- backend/src/Models/Users/user.model.ts
+++ backend/src/Models/Users/user.model.ts
@@ -7,7 +7,7 @@ export const findUserByEmail = async (username: string) => {
         console.log(username);
         // Attempt to find the user by email and return the information
         const foundUser = await db<Promise<User>>('users')
-            .select('username')
+            .select('id', 'username')
             .where('username', username); // Changed to 'email'
         return foundUser as User[];
     } catch (error) {
@@ -15,9 +15,6 @@ export const findUserByEmail = async (username: string) => {
         throw new Error('Failed to fetch user by email');
     }
 };
-
-
-
 
 export const createNewUserProfile = async (username: string) => {
     // Check if the username is valid
@@ -48,3 +45,33 @@ export const createNewUserProfile = async (username: string) => {
     }
 };
 
+// This function will be used to update the location of a user
+export const updateUserLocation = async (userEmail: string, latitude: number, longitude: number) => {
+    try {
+        // Get the ID of the user so we can search the database
+        const foundUser = await findUserByEmail(userEmail);
+
+        // Throw an error if that user isn't in the DB
+        if (!foundUser[0]) {
+            throw new Error('User not found');
+        }
+
+        // Update the location of the user who matches the ID from findUserByName.
+        const updatedUser = await db<User>('users')
+            .where('id', foundUser[0].id)
+            .update({
+                latitude: latitude,
+                longitude: longitude,
+            })
+            .returning(['id', 'username', 'userPostIds', 'created_at', 'latitude', 'longitude', 'email']);
+
+        console.log(`Successfully updated location of user ${foundUser[0].username}`);
+
+        // Return the first user in the user array created above
+        return updatedUser[0];
+        
+    } catch (error) {
+        console.error('Error updating user location', error);
+        throw new Error('Failed to update user location');
+    }
+}
