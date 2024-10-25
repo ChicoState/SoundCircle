dd969b9af7b748ec09806a307d87761be8994296
Austin <kleineckeaustin@gmail.com>
2024-10-25 12:01:43 -0700

    Added Google API to packages. Created new page for Feed.

--- frontend/src/Pages/Feed.tsx
+++ frontend/src/Pages/Feed.tsx
@@ -7,6 +7,7 @@ const Home = () => {
         <div className="min-h-screen flex-col overflow-y-hidden">
             {/*Main Page Container*/}
             <div>
+            {/*<div className="bg-gray-500 flex flex-col items-center py-5">*/}
                 {/*Header Section*/}
                 <Header/>
             </div>
@@ -17,7 +18,7 @@ const Home = () => {
                 <div className="bg-gray-900 w-3/4 flex flex-grow">
                     {/*Left Column Section*/}
                     <ul className="w-full p-5 items-center text-center">
-                        
+                        <FeedContainer/>
                     </ul>
                 </div>
 
