c7d8105ba19053c3d204f145c3c747cf1181a314
alexb02h <alexbaroun028@gmail.com>
2024-10-28 12:28:49 -0700

    random

--- frontend/src/PageElements/Home/UserPage/UserIcon.tsx
+++ frontend/src/PageElements/Home/UserPage/UserIcon.tsx
@@ -1,12 +1,12 @@
-import { useState,useEffect } from "react";
 import UserIcon from "../../../Components/UserIconTemp.png";
+import RandomGavinNewsom from "../../../Components/download.jpg";
 import './UserIcon.css';
 
 
 const UserImage: React.FC = () => {
     return(
         <div className="User-Image"> 
-            <img src={UserIcon} alt="SC Logo" className="circlular-user" />
+            <img src={RandomGavinNewsom} alt="SC Logo" className="circlular-user" />
         </div>
     );
 };
