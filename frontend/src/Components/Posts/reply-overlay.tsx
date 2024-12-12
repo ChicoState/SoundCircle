d2fcc9eddd12ec4ff51074a0eb53df634730ee17
tooth-juice <kleineckeaustin@gmail.com>
2024-12-12 00:05:29 -0800

    Created potentially unneeded globals file. WIP functionality for reply overlay user input and identification.

--- frontend/src/Components/Posts/reply-overlay.tsx
+++ frontend/src/Components/Posts/reply-overlay.tsx
@@ -1,12 +1,38 @@
+// TO-DO: USER LINKS
+
+import { useEffect, useRef, useState } from "react";
 import Overlay from "../Reusable/overlayBox";
 import NavigationButton_UserProfilePic from "../Universal/NavigationButton_UserProfilePic";
 import { PostProperties } from "./post-main";
+import { MAX_COMMENT_CHARACTERS } from "../../globals";
 
 function ReplyOverlay({ isVisible, onOutsidePress, replyInformation}: {isVisible: boolean, onOutsidePress: () => void, replyInformation?: PostProperties }) {
+    // Info for user input
+    const placeholder = "Enter your reply..."
+    const [text, setText] = useState('');
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+    const [isFocused, setIsFocused] = useState(false);
+
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const value = event.target.value
+        if (value.length <= MAX_COMMENT_CHARACTERS) {
+            setText(value)
+        }
+    }
+
+    // Update the size of the text box as we add more text
+    useEffect(() => {
+        if (textAreaRef.current) {
+            textAreaRef.current.style.height = 'auto'
+            textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`
+        }
+    }, [text])
+
 
     return (
         <Overlay 
-        classNameBox='bg-white rounded-lg shadow-lg w-[30rem] h-[30rem]' 
+        classNameBox='bg-white rounded-lg shadow-lg w-[30rem]' 
         isVisible={isVisible} 
         onOutsidePress={onOutsidePress}
         >
@@ -15,7 +41,7 @@ function ReplyOverlay({ isVisible, onOutsidePress, replyInformation}: {isVisible
                 <button className='' onClick={onOutsidePress}>
                     Cancel
                 </button>
-                <button className=''>
+                <button className='text-gray-400'>
                     Reply
                 </button>
             </div>
@@ -31,17 +57,41 @@ function ReplyOverlay({ isVisible, onOutsidePress, replyInformation}: {isVisible
                     />
                     <p className='text-center'>{replyInformation?.username}</p>
                 </div>
-                <div className='w-[20rem]'>
+                <div className='w-[23rem]'>
                     {replyInformation?.post_content}
                 </div>
             </div>
 
             {/* Separation Line */}
-            <hr className='mx-auto w-[28rem] h-px my-2 bg-gray-300 border-0'/>
+            <div className='pt-4'>
+                <text className='ml-2 text-xs italic'>
+                    Replying to {replyInformation?.username}
+                </text>
+                <hr className='mx-auto w-[28rem] h-px my-2 bg-gray-300 border-0'/>
+            </div>
+            
 
             {/* Input area */}
-            <div>
-
+            <div className='flex items-start p-w pt-4 pb-2'>
+                <div className='flex flex-col ml-2 mr-6'>
+                    <NavigationButton_UserProfilePic
+                    className="w-[4rem] h-[4rem] rounded-full"
+                    username="Default User"
+                    profileImage={process.env.REACT_APP_PLACEHOLDER_USER}
+                    navigationPath={`/User`}
+                    />
+                    <p className='text-sm'>Default User</p>
+                </div>
+                <textarea
+                className='w-[23rem] resize-none overflow-y-auto hover:outline-none focus:outline-none'
+                ref={textAreaRef}
+                value={text}
+                onChange={handleInputChange}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                placeholder={placeholder}
+                rows={4}
+                />
             </div>
         </Overlay>
     )
